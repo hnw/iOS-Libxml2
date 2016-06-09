1a979a0a001172beb65c154df2d9c727bde81321
Yoshio HANAWA <y@hnw.jp>
2016-06-09 15:50:40 +0900

    Initial commit

---
+++ Libxml2/Libxml2.h
@@ -0,0 +1,19 @@
+//
+//  Libxml2.h
+//  Libxml2
+//
+//  Created by hanawa-y on 2016/06/09.
+//  Copyright © 2016年 hnw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Libxml2.
+FOUNDATION_EXPORT double Libxml2VersionNumber;
+
+//! Project version string for Libxml2.
+FOUNDATION_EXPORT const unsigned char Libxml2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Libxml2/PublicHeader.h>
+
+
